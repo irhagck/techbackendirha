cfadbea3f42b9b17b6565236866c9f8c6168452f
aleesha079912 <alishabsit@gmail.com>
2026-04-26 11:45:06 +0500

    update

--- routes/api.php
+++ routes/api.php
@@ -26,6 +26,6 @@ use Illuminate\Http\Request;
 // use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
-Route::post('/login', [UserController::class, 'login']);
+// Route::post('/login', [UserController::class, 'login']);
 Route::get('/welcome2', [UserController::class, 'welcome2']);
  
